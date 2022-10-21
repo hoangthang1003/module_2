3aeb5c3f099005df955f22b924017fdce503e80d
hoangthang1003 <110215092+hoangthang1003@users.noreply.github.com>
2022-10-21 07:30:18 +0700

    nopbai

---
+++ src/ss12_tree_map/exercise/ex1/ProducManager.java
@@ -0,0 +1,75 @@
+package ss12_tree_map.exercise.ex1;
+
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ProducManager extends Product {
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<Product> productsArrayList = new ArrayList<>();
+    Product product1 = new Product(1, "Táo", 5);
+    Product product2 = new Product(2, "Chuối", 2);
+    Product product3 = new Product(1, "Dưa hấu", 9);
+
+    {
+        productsArrayList.add(product1);
+        productsArrayList.add(product2);
+        productsArrayList.add(product3);
+    }
+
+    public void display() {
+        System.out.println(productsArrayList);
+    }
+
+    public void add() {
+        Product product = new Product();
+        System.out.println("Mời bạn nhập id");
+        product.setId(Integer.parseInt(scanner.nextLine()));
+        System.out.println("Mời bạn nhập tên");
+        product.setName(scanner.nextLine());
+        System.out.println("Mời bạn nhập giá");
+        product.setPrice(Double.parseDouble(scanner.nextLine()));
+        productsArrayList.add(product);
+        display();
+    }
+
+    public void deleteProduct() {
+        System.out.println("Nhập id sản phẩm muốn xóa");
+        int id = Integer.parseInt(scanner.nextLine());
+        for (Product item : productsArrayList) {
+            if (item.getId() == id) {
+                productsArrayList.remove(item);
+                display();
+                return;
+            }
+        }
+    }
+
+    public void edit() {
+        System.out.println("nhập id sản phâm bạn muốn sửa vào");
+        int id = Integer.parseInt(scanner.nextLine());
+        for (Product item : productsArrayList) {
+            if (item.getId() == id) {
+                System.out.print("Moi ban nhap lai id: ");
+                item.setId(Integer.parseInt(scanner.nextLine()));
+                System.out.print("Moi ban nhap lai ten san pham: ");
+                item.setName(scanner.nextLine());
+                System.out.print("Moi ban nhap lai gia");
+                item.setPrice(Double.parseDouble(scanner.nextLine()));
+                display();
+                break;
+            }
+        }
+    }
+
+    public void search() {
+        System.out.println("Nhập sản phẩm bạn muốn tìm kiếm");
+        String name = scanner.nextLine();
+        for (Product item : productsArrayList) {
+            if (item.getName() == name) {
+                System.out.println(item.toString());
+            }
+        }
+    }
+}
---
+++ src/ss12_tree_map/exercise/ex1/TestProduct.java
@@ -0,0 +1,50 @@
+package ss12_tree_map.exercise.ex1;
+
+import case_study.controller.*;
+
+import java.util.Scanner;
+
+public class TestProduct {
+    public static void main(String[] args) {
+        ProducManager productManager = new ProducManager();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào 1 số");
+        do {
+            System.out.println("------CHOICE------\n" +
+                    "1.Add product\n" +
+                    "2.Edit product\n" +
+                    "3.Delete product\n" +
+                    "4.Display products\n " +
+                    "5.Sort product\n" +
+                    "6.Exit");
+            System.out.print("Choice of you : ");
+            int choose = Integer.parseInt(scanner.nextLine());
+            if (choose < 1 || choose > 6) {
+                System.out.println("Nhập lại số từ 1 đến 6");
+                continue;
+            }
+            switch (choose) {
+
+                case 1:
+                    productManager.add();
+                    break;
+                case 2:
+                    productManager.edit();
+                    break;
+                case 3:
+                    productManager.deleteProduct();
+                    break;
+                case 4:
+                    productManager.display();
+                    break;
+                case 5:
+                    productManager.search();
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    System.exit(1);
+            }
+        } while (true);
+    }
+}
